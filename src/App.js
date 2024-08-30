2e1a1eabf7be4e888545e6fd848882c88a329bd0
Náder W. S. Sírio <nadersirio@gmail.com>
2024-08-29 23:35:39 -0300

    add: api and landing page mapping json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { card, AddItemOnCart } from './script';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+          <a href="/">Store</a>
+          <p>Cart: ${''} Item(s) - ${''}</p>
+        </h1>
       </header>
+      <main>
+        <div className='Cards'>
+          <ul>
+            {card.map((item) => (
+            <li key={item.sku} className='Card'>
+              <img src={item.image} alt="Imagem de Produto em Cartaz"/>
+              <h2>${item.priceSpecification.price}</h2>
+              <h3>Old Price: ${item.priceSpecification.oldPrice}</h3>
+              <p>{item.name}</p>
+              {item.priceSpecification.discount && (
+                <p>Discount: {item.priceSpecification.discount}%</p>
+              )}
+              <div className="CartButton">
+                <p>🛒</p>
+                <button onClick={() => AddItemOnCart(item)}>ADD TO 
+                CART</button>
+              </div>
+            </li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/script.js
@@ -0,0 +1,9 @@
+import api from './api.json';
+
+const card = api;
+
+function AddItemOnCart(item){
+  console.log('adicinado no carrinho');
+}
+
+export { card, AddItemOnCart };
