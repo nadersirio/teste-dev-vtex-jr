3f52329c7989418503aedafcd0a7a59c49312d39
Náder W. S. Sírio <nadersirio@gmail.com>
2024-08-30 22:35:05 -0300

    Add: logic and states increment

--- src/App.js
+++ src/App.js
@@ -1,33 +1,65 @@
 import './App.css';
-import { card, AddItemOnCart } from './script';
+import api from './api.json';
+import React, { useState } from 'react';
 
 function App() {
+  const [Cart, setCart] = useState([]);
+  const [QtdItem, setQtdItem] = useState(0);
+  const [ItemsValue, setItemsValue] = useState(0);
+
+  function AddItemOnCart(price, index) {
+    let newCart = [...Cart];
+    let ItemOnCart = newCart.find(item => item.index === index);
+
+    if (ItemOnCart) {
+      ItemOnCart.Qtd += 1;
+    } else {
+      newCart.push({ index: index, price: price, Qtd: 1 });
+    }
+
+    setCart(newCart);
+
+    let newQtdItem = newCart.reduce((total, item) => total + item.Qtd, 0);
+    let newItemValue = newCart.reduce((total, item) => total + item.price * item.Qtd, 0);
+
+    setQtdItem(newQtdItem);
+    setItemsValue(newItemValue);
+  }
+
   return (
     <div className="App">
       <header>
         <h1>
-          <a href="/">Store</a>
-          <p>Cart: ${''} Item(s) - ${''}</p>
+          <a href="/">STORE</a>
+          <div className="CartButton">
+            <p>🛒</p>
+            <button>Cart: {QtdItem} Item(s) - ${ItemsValue.toFixed(2)}</button>
+          </div>
         </h1>
       </header>
       <main>
-        <div className='Cards'>
+        <div className="Cards">
           <ul>
-            {card.map((item) => (
-            <li key={item.sku} className='Card'>
-              <img src={item.image} alt="Imagem de Produto em Cartaz"/>
-              <h2>${item.priceSpecification.price}</h2>
-              <h3>Old Price: ${item.priceSpecification.oldPrice}</h3>
-              <p>{item.name}</p>
-              {item.priceSpecification.discount && (
-                <p>Discount: {item.priceSpecification.discount}%</p>
-              )}
-              <div className="CartButton">
-                <p>🛒</p>
-                <button onClick={() => AddItemOnCart(item)}>ADD TO 
-                CART</button>
-              </div>
-            </li>
+            {api.map((item) => (
+              <li key={item.sku} className="Card">
+                <img src={item.image} alt="Imagem de Produto em Cartaz" />
+                {item.priceSpecification.oldPrice && (
+                  <h3>TO: ${item.priceSpecification.oldPrice} FOR:</h3>
+                )}
+                <h2>${item.priceSpecification.price}</h2>
+                <p>{item.name}</p>
+                {item.priceSpecification.discount && (
+                  <p>Discount: {item.priceSpecification.discount}%</p>
+                )}
+                <div className="CartButton">
+                  <p>🛒</p>
+                  <button
+                    onClick={() => AddItemOnCart(item.priceSpecification.price, item.sku)}
+                  >
+                    ADD TO CART
+                  </button>
+                </div>
+              </li>
             ))}
           </ul>
         </div>
--- src/script.js
+++
@@ -1,9 +0,0 @@
-import api from './api.json';
-
-const card = api;
-
-function AddItemOnCart(item){
-  console.log('adicinado no carrinho');
-}
-
-export { card, AddItemOnCart };
